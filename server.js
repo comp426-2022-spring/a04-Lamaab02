021695f19bfa7bd48794a0b9642675f948000067
Lama Abed <lamaab@email.unc.edu>
2022-04-19 22:33:23 -0400

    ffffsss

--- server.js
+++ server.js
@@ -28,27 +28,30 @@ if (args.help) {
     exit(0);
 }
 
+const database = require('./database')
 
-const logdb = require('./database')
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-
+// Start an app server
 
 const port = args.port || 5555
 
-const server = app.listen(port, () =>{
-    console.log('App is running on port %PORT%'.replace('%PORT%', port))
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
 
 
-app.get('/app/', (req, res)=>{
-    res.statusCode = 200;
-    res.statusMessage = "OK";
-    res.writeHead(res.statusCode, {'content-Type' : 'text/plain'});
-    res.end(res.statusCode + ' ' + res.statusMessage)
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+        res.statusCode = 200;
+    // Respind with status message 'OK'
+        res.statusMessage = "OK";
+        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain '});
+        res.end(res.statusCode + ' ' + res.statusMessage);
 });
 
+// Middleware function
 app.use( (req, res, next) => {
     let logdata = {
         remoteaddr: req.ip,
@@ -61,24 +64,23 @@ app.use( (req, res, next) => {
         status: res.statusCode,
         referer: req.headers['referer'],
         useragent: req.headers['user-agent']
-    
-        }
-        const stmt = logdb.prepare(`INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`);
-        stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
-        next();
+    }
+
+    const stmt = database.prepare(`INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`);
+    stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+
+    next();
 })
 
-if(args.debug == true){
-    app.get('/app/log/access', (req, res) =>{
-        const select = logdb.prepare('SELECT * FROM accesslog').all();
+if (args.debug == true) {
+    app.get('/app/log/access', (req, res) => {
+        const select = database.prepare('SELECT * FROM accesslog').all();
         res.status(200).json(select);
-
     })
 
-    app.get('/app/error', (req, res) =>{
+    app.get('/app/error', (req, res) => {
         throw new Error('Error test successful.')
     })
-
 }
 
 if (args.log == true) {
@@ -86,19 +88,8 @@ if (args.log == true) {
     app.use(morgan('FORMAT', { stream: WRITESTREAM }));
 }
 
+// Default response for any other request 
 
 app.use(function(req, res) {
-    res.status(404).send('404 NOT FOUND')
-  });
-
-
-
-
-
-
-
-
-const accessLog = fs.createWriteStream('access.log', { flags: 'a' })
-
-app.use(morgan('combined', { stream: accessLog }))
-
+  res.status(404).send('404 NOT FOUND')
+});
